6651ff32d73a302a24f7124687b8eeb3a2ff3061
CleverSantoro <cleversantoro@gmail.com>
2025-10-02 22:20:24 -0300

    cliente type

---
+++ src/app/types/oficina/cliente.type.ts
@@ -0,0 +1,15 @@
+﻿export type Cliente = {
+  id?: string
+  nome?: string
+  documento?: string
+  telefone?: string
+  email?: string
+  createdAt?: string
+}
+
+export interface Paginacao<T> {
+  items: T[]
+  total: number
+  page: number
+  pageSize: number
+}
